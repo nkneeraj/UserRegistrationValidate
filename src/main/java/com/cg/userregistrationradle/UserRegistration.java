2f585619c0cefa6173f046e67bfac04e358442fe
nkneeraj <nkneerj641@gmail.com>
2020-09-30 16:01:18 +0530

    [NK]Added UC1: User name with first capital character and min 3 length.

--- src/main/java/com/cg/userregistrationradle/UserRegistration.java
+++ src/main/java/com/cg/userregistrationradle/UserRegistration.java
@@ -1,7 +1,29 @@
 package com.cg.userregistrationradle;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	private static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the User Registration portal");
+		FirstName();
+	}
+
+	public static void PatternMatcher(Pattern pattern, String string) {
+		Matcher matcher = pattern.matcher(string);
+		if (matcher.find())
+			System.out.println("Valid Entry");
+		else
+			System.out.println("Invalid entry");
+	}
+
+	public static void FirstName() {
+		System.out.println("Enter a user name with first capital character and min 3 length:");
+		String firstName = sc.nextLine();
+		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{2,}$");
+		PatternMatcher(pattern,firstName);
 	}
 }
