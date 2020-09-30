a018fbcb685655b57c497877c3dc05cca178b17f
nkneeraj <nkneerj641@gmail.com>
2020-09-30 15:50:29 +0530

    [NK]Added: Welcome to the User Registration portal.

---
+++ src/main/java/com/cg/userregistrationradle/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.cg.userregistrationradle;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the User Registration portal");
+	}
+}
