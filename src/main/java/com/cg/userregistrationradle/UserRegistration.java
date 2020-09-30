47de1f823075e3d9c3e1e4f49c6fcb451c589414
nkneeraj <nkneerj641@gmail.com>
2020-09-30 16:45:35 +0530

    [NK]Modified UC5: UC6 case deleted from UC5.

--- src/main/java/com/cg/userregistrationradle/UserRegistration.java
+++ src/main/java/com/cg/userregistrationradle/UserRegistration.java
@@ -59,7 +59,7 @@ public class UserRegistration {
 	{
 		System.out.println("Enter Password with min length 8 and one uppercase");
 		String Password = sc.nextLine();
-		Pattern pattern = Pattern.compile("(?=.*[A-Z])([a-zA-Z0-9]){8,16}$");
+		Pattern pattern = Pattern.compile("([a-zA-Z0-9]){8,16}$");
 		PatternMatcher(pattern,Password);
 	}
 }
